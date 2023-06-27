84124999dc4315680923d6ef0988ec7580142287
Prajwal-Krishna-Bhat <133648728+Prajwal-Krishna-Bhat@users.noreply.github.com>
2023-06-27 10:59:50 +0000

    it_works

--- Jobscheduling.py
+++ Jobscheduling.py
@@ -1,17 +1,17 @@
 class Job:
-    def __init__(self,taskid,deadline,profit):
-        self.taskid=taskid
-        self.deadline=deadline
-        self.profit=profit
+    def __init__(self,Id,dl,p):
+        self.taskid=Id
+        self.deadline=dl
+        self.profit=p
 def schedulejobs(jobs,T):
     profit=0
     slot=[-1]*T
-    jobs.sort(key=lambda x: x.profit,reverse=True)
-    for job in jobs:
-        for j in reversed(range(min(T,job.deadline))):
+    jobs.sort(key=lambda x: x.profit, reverse=True)
+    for jobs in jobs:
+        for j in reversed(range(min(T,jobs.deadline))):
             if slot[j]==-1:
-                slot[j]=job.taskid
-                profit +=job.profit
+                slot[j]=jobs.taskid
+                profit +=jobs.profit
                 break
     print("The scheduled jobs are",list(filter(lambda x:x!=-1,slot)))
     print("the total profit earned is",profit)
@@ -20,8 +20,8 @@ if __name__=='__main__':
     jobs=[]
     for i in range(n):
         taskid=input("Enter task ID for job{}:".format(i+1))
-        deadline=input("Enter deadlinefor job{}:".format(i+1))
-        profit=input("Enter profit for job{}:".format(i+1))
+        deadline=int(input("Enter deadline for job{}:".format(i+1)))
+        profit=int(input("Enter profit for job{}:".format(i+1)))
         jobs.append(Job(taskid,deadline,profit))
     T=int(input("Enter the total time:"))
     schedulejobs(jobs,T)
