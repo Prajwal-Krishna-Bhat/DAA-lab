55b6fcfd219fdc12e57f351eeebd6bb6c351443a
Prajwal-Krishna-Bhat <133648728+Prajwal-Krishna-Bhat@users.noreply.github.com>
2023-07-09 04:37:45 +0000

    graph still not possible

--- merge.py
+++ merge.py
@@ -1,38 +1,36 @@
 import time
-from numpy.random import randint
 import matplotlib.pyplot as plt
-def mergeSort(array):
-    if len(array)>1:
-        r=len(array)//2
-        L=array[:r]
-        M=array[r:]
-        mergeSort(L)
-        mergeSort(M)
+from numpy.random import randint
+def mergesort(aray):
+    if len(aray)>1:
+        r=len(aray)//2
+        L=aray[:r]
+        M=aray[r:]
+        mergesort(L)
+        mergesort(M)
         i=j=k=0
         while i<len(L) and j<len(M):
             if L[i]<M[j]:
-                array[k]=L[i]
+                aray[k]=L[i]
                 i+=1
             else:
-                array[k]=M[j]
+                aray[k]=M[j]
                 j+=1
             k+=1
         while i<len(L):
-            array[k]=L[i]
+            aray[k]=L[i]
             i+=1
             k+=1
         while j<len(M):
-            array[k]=M[j]
+            aray[k]=M[j]
             j+=1
             k+=1
-def printlist(array):
-    for i in range(len(array)):
-        print(array[i],end="")
+
+def printlist(aray):
+    for i in range(len(aray)):
+        print(aray[i],end=" ")
     print()
-array=[6,5,12,10,9,1]
-mergeSort(array)
-print("Sorted array is:")
-printlist(array)
+
 def partition(array,low,high):
     pivot=array[high]
     i=low-1
@@ -42,18 +40,13 @@ def partition(array,low,high):
             (array[i],array[j])=(array[j],array[i])
     (array[i+1],array[high])=(array[high],array[i+1])
     return i+1
+
 def quicksort(array,low,high):
     if low<high:
         pi=partition(array,low,high)
         quicksort(array,low,pi-1)
         quicksort(array,pi+1,high)
-data=[8,7,2,1,0,9,6]
-print("unsorted array:")
-print(data)
-size=len(data)
-quicksort(data,0,size-1)
-print('Sorted array in acending order:')
-print(data)
+
 def selectionsort(array,size):
     for step in range(size):
         min_idx=step
@@ -61,45 +54,47 @@ def selectionsort(array,size):
             if array[i]<array[min_idx]:
                 min_idx=i
         (array[step],array[min_idx])=(array[min_idx],array[step])
+
 def read_input():
     a=[]
-    n=int(input("Enter no of tv channels"))
-    print("Enter no of viewers")
+    n=int(input("Enter the number of TV channels: "))
+    print("Enter the numebr of viewers: ")
     for i in range(0,n):
         l=int(input())
         a.append(l)
-    return a
+    return a;
+
 elements=list()
 times=list()
 global labeldata
-print("1.MergeSort 2.QuickSort 3.SelectionSort")
-ch=int(input("Enter the choice"))
+print("1.MergeSort\n2.QuickSort\n3.SelectionSort\n")
+ch=int(input("Enter the choice:"))
 if(ch==1):
     array=read_input()
-    mergeSort(array)
-    labeldata="MergeSort"
-    print('Sorted array is')
+    mergesort(array)
+    labeldata="Mergesort"
+    print('sorted data: ')
     print(array)
 if(ch==2):
     array=read_input()
     size=len(array)
+    labeldata="Quicksort"
     quicksort(array,0,size-1)
-    labeldata="Quick Sort"
-    print('Sorted array is')
+    print('sorted data: ')
     print(array)
 if(ch==3):
     array=read_input()
     size=len(array)
+    labeldata="Selectionsort"
     selectionsort(array,size)
-    labeldata="Quick Sort"
-    print('Sorted array is')
+    print('sorted data: ')
     print(array)
-print("***********Running Time Analysis**********")
+print("********************************RUNNING TIME ANALYSIS**********************************")
 for i in range(1,10):
     array=randint(0,1000*i,1000*i)
     start=time.time()
     if ch==1:
-        mergeSort(array)
+        mergesort(array)
     elif ch==2:
         size=len(array)
         quicksort(array,0,size-1)
@@ -107,11 +102,12 @@ for i in range(1,10):
         size=len(array)
         selectionsort(array,size)
     end=time.time()
-    print(len(array),"elements sorted by",labeldata,end-start)
-    element.append(len(array))
+    print(len(array)," Elementa sorted by ",labeldata," in ",end-start)
+    elements.append(len(array))
     times.append(end-start)
-plt.xlabel('List length')
-plt.ylabel('Time complexity')
+
+plt.xlabel('list length')
+plt.ylabel('time complxty')
 plt.plot(elements,times,label=labeldata)
 plt.grid()
 plt.legend()
