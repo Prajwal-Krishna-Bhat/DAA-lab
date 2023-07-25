e41c43dd905e1a5b927c30a2ef97afc1fb8682e7
Prajwal-Krishna-Bhat <133648728+Prajwal-Krishna-Bhat@users.noreply.github.com>
2023-07-25 09:58:22 +0000

    done

---
+++ Lab9.py
@@ -0,0 +1,61 @@
+# Design and imoplement for a given chess board having NxN cells. Place N queens on the board such that no queen attacks any other queen. if it is possible to place all the N queens in such a way, then print N lines having N queens. If there is more than one sulution of placing the queens, oprint them all. If it is not possible to place all the of the N queens in the desired way, then print "Not possible"
+
+def is_safe(board, row, col, N):
+    for i in range(col):
+        if board[row][i]==1:
+            return False
+    i=row
+    j=col
+    while i>=0 and j>=0:
+        if board[i][j]==1:
+            return False
+        i-=1
+        j-=1
+    i=row
+    j=col
+    while j>=0 and i<N:
+        if board[i][j]==1:
+            return False
+        i+=1
+        j-=1
+    return True
+def solve_n_queens_util(board, col, N, solutions):
+    if col==N:
+        solution=[]
+        for i in range(N):
+            row=[]
+            for j in range(N):
+                row.append(board[i][j])
+            solution.append(row)
+        solutions.append(solution)
+        return
+    for i in range(N):
+        if is_safe(board, i, col, N):
+            board[i][col]=1
+            solve_n_queens_util(board, col+1, N, solutions)
+            board[i][col]=0
+def solve_n_queens(N):
+    board=[[0]*N for _ in range(N)]
+    solutions=[]
+    solve_n_queens_util(board,0,N,solutions)
+    if len(solutions)==0:
+        print("Not possible")
+    else:
+        for solution in solutions:
+            for row in solution:
+                for cell in row:
+                    print("Q"if cell==1 else ".",end=" ")
+                print( )
+            print( )
+N=int(input("Enter the no of queens:"))
+solve_n_queens(N)
+# Enter the no of queens:4
+# . . Q . 
+# Q . . . 
+# . . . Q 
+# . Q . . 
+
+# . Q . . 
+# . . . Q 
+# Q . . . 
+# . . Q . 
